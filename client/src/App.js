b9bc1d5594ccaa9838fd181a5ccf28381cb1993f
Satyam Baranwal <satyamkriitk@gmail.com>
2023-07-19 01:10:44 +0530

    adding client as well

---
+++ client/src/App.js
@@ -0,0 +1,62 @@
+
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+            <Link to="/">Home</Link>
+            <Link to="/otherpage">Other Page</Link>
+            {/* <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a> */}
+          </header>
+          <div>
+            <Route exact path="/" component={Fib}/>
+            <Route path="/otherpage" component={OtherPage}/>
+          </div>
+        </div>
+
+      </Router>
+    );
+  }
+}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from "react";
+import { Link } from "react-router-dom";
+export default () => {
+  return (
+    <div>
+      Other Page!
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
